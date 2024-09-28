8e0c7f9a21df619eb47f06531d9a2ca7518388ab
Hampus <hampus03brink@gmail.com>
2024-09-28 21:51:49 +0200

    font and damage

--- Assets/Source/Scripts/Weapons/BaseWeapon.cs
+++ Assets/Source/Scripts/Weapons/BaseWeapon.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class BaseWeapon : MonoBehaviour
@@ -11,14 +13,28 @@ public class BaseWeapon : MonoBehaviour
     public float baseDamage = 10f;
     public float baseAttackSpeed = 2f;
 
-    public void WeaponPerformAttack()
+    public void WeaponPerformAttack(float attackDuration)
     {
-        //ActivateAttack();
+        StartCoroutine(ActivateAttack(attackDuration));
     }
     
-    // private IEnumerator ActivateAttack()
-    // {
-    //     weaponCol.enabled = true;
-    //     //yield return new WaitForSeconds()
-    // }
+    private IEnumerator ActivateAttack(float attackDuration)
+    {
+        weaponCol.enabled = true;
+        yield return new WaitForSeconds(attackDuration);
+        // ReSharper disable once Unity.InefficientPropertyAccess
+        weaponCol.enabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.TryGetComponent(out PhotonView photonView))
+        {
+            if (photonView.TryGetComponent(out IDamagable damagable))
+            {
+                photonView.RPC(nameof(damagable.RPC_TakeDamage),RpcTarget.All,photonView.ViewID,baseDamage);
+                weaponCol.enabled = false;
+            }
+        }
+    }
 }
