7c8b11e4e183b545b08b31b38da198f292ae1bc7
Hampus <hampus03brink@gmail.com>
2024-09-29 12:49:36 +0200

    Description for items

--- Assets/Source/PowerUpSOs/ItemButton.cs
+++ Assets/Source/PowerUpSOs/ItemButton.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,14 +10,17 @@ public class ItemButton : MonoBehaviour
 {
     public PowerupSO powerUp;
     [SerializeField] private Image _display;
+    [SerializeField] private TMP_Text _displayText;
     private void Start()
     {
         _display.sprite = powerUp.display;
+        _displayText.text = powerUp.description;
     }
 
     public void UpdateItem()
     {
         _display.sprite = powerUp.display;
+        _displayText.text = powerUp.description;
     }
 
     public void OnItemPicked()
