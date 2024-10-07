7be88babbc1a97c00d49187ee218bc577faa27a2
Luca Belluso <lucabelluso123@gmail.com>
2024-10-07 18:49:09 +0200

    Moved stuff from base weapon to sword

--- Assets/Scripts/Weapons/BaseWeapon.cs
+++ Assets/Scripts/Weapons/BaseWeapon.cs
@@ -9,38 +9,34 @@ public class BaseWeapon : MonoBehaviourPunCallbacks
 {
 
     // Components
-    public BoxCollider2D weaponCol;
     public Animator animator;
     //public SpriteRenderer weaponGFX;
     
-    [Header("Stats")]
+    [Header("Weapon Stats")]
     public float baseDamage = 10f;
     public float baseAttackSpeed = 2f;
-    public float baseWeaponLength = 1.4f;
+    
 
-    private float _multipliedDamage;
-    private float _multipliedWeaponLength;
+    protected float _multipliedDamage;
 
     [HideInInspector] public bool isAlreadyAttacking;
 
-    private void Start()
+    protected virtual void Start()
     {
-        GetComponents();
+        BaseWeaponGetComponents();
         _multipliedDamage = baseDamage;
-        _multipliedWeaponLength = baseWeaponLength;
     }
 
-    private void GetComponents()
+    private void BaseWeaponGetComponents()
     {
-        animator = gameObject.GetComponent<Animator>();
+        animator = gameObject.GetComponent<Animator>(); // doesn't work?? might have to assign through unity
     }
 
-    public void WeaponPerformAttack(float attackDuration)
+    public virtual void WeaponPerformAttack(float attackDuration)
     {
         if(isAlreadyAttacking) return;
         Debug.Log("Tries to trigger attack");
-        animator.SetTrigger("Attack");
-        StartCoroutine(ActivateAttack(attackDuration));
+        
     }
 
     public void UpdateAttackDamage(float multiplier)
@@ -48,33 +44,9 @@ public class BaseWeapon : MonoBehaviourPunCallbacks
         _multipliedDamage = baseDamage * multiplier;
     }
 
-    public void UpdateWeaponLength(float multiplier)
-    {
-        _multipliedWeaponLength = baseWeaponLength * multiplier;
-        weaponCol.transform.localScale = new Vector3(weaponCol.transform.localScale.x, _multipliedWeaponLength, weaponCol.transform.localScale.z);
-    }
     
-    private IEnumerator ActivateAttack(float attackDuration)
-    {
-        isAlreadyAttacking = true;
-        weaponCol.enabled = true;
-        yield return new WaitForSeconds(attackDuration);
-        // ReSharper disable once Unity.InefficientPropertyAccess
-        weaponCol.enabled = false;
-        isAlreadyAttacking = false;
-    }
+    
+    
 
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if(!GameManager.Instance.GameStarted) return;
-        if (col.TryGetComponent(out PhotonView pv))
-        {
-            if (pv.TryGetComponent(out IDamagable damagable))
-            {
-                if(!photonView.IsMine) return;
-                pv.RPC(nameof(damagable.RPC_TakeDamage),RpcTarget.All,pv.ViewID,_multipliedDamage);
-                weaponCol.enabled = false;
-            }
-        }
-    }
+    
 }
--- Assets/Scripts/Weapons/Sword.cs
+++ Assets/Scripts/Weapons/Sword.cs
@@ -1,18 +1,79 @@
  using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+
 
 public class Sword : BaseWeapon
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Sword Settings")]
+    public GameObject weaponColObject;
+    private Transform weaponColTransform;
+    private BoxCollider2D weaponCol;
+    [SerializeField] private float sweepStartAngle = 90f;
+    [SerializeField] private float sweepEndAngle = -90f;
+    
+    [Header("Sword Specific Stats")]
+    public float baseWeaponLength = 1.4f;
+
+    protected override void Start()
     {
-        
+        base.Start();
+        SwordGetComponents();
+        UpdateWeaponLength(1.0f); // fix this later
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SwordGetComponents()
     {
-        
+        weaponColTransform = weaponColObject.transform;
+        weaponCol = weaponColObject.GetComponent<BoxCollider2D>();
     }
+    
+    
+    private float _multipliedWeaponLength;
+    public void UpdateWeaponLength(float multiplier)
+    {
+        _multipliedWeaponLength = baseWeaponLength * multiplier;
+        weaponCol.transform.localScale = new Vector3(weaponCol.transform.localScale.x, _multipliedWeaponLength, weaponCol.transform.localScale.z);
+    }
+
+    public override void WeaponPerformAttack(float attackDuration)
+    {
+        base.WeaponPerformAttack(attackDuration);
+        animator.SetTrigger("Attack");
+        StartCoroutine(ActivateAttack(attackDuration));
+    }
+    
+    private IEnumerator ActivateAttack(float attackDuration)
+    {
+        // ReSharper disable once Unity.InefficientPropertyAccess
+        isAlreadyAttacking = true;
+        weaponCol.enabled = true;
+        
+        float attackTimeElapsed = 0f;
+        while (attackTimeElapsed < attackDuration)
+        {
+            attackTimeElapsed += Time.deltaTime;
+            float t = attackTimeElapsed / attackDuration;
+            weaponColTransform.localRotation = Quaternion.Euler(0, 0, Mathf.Lerp(sweepStartAngle, sweepEndAngle, t));
+            yield return null;
+        }
+        
+        weaponCol.enabled = false;
+        isAlreadyAttacking = false;
+    }
+    
+    // private void OnTriggerEnter2D(Collider2D col)
+    // {
+    //     if(!GameManager.Instance.GameStarted) return;
+    //     if (col.TryGetComponent(out PhotonView pv))
+    //     {
+    //         if (pv.TryGetComponent(out IDamagable damagable))
+    //         {
+    //             if(!photonView.IsMine) return;
+    //             pv.RPC(nameof(damagable.RPC_TakeDamage),RpcTarget.All,pv.ViewID,_multipliedDamage);
+    //             weaponCol.enabled = false;
+    //         }
+    //     }
+    // }
 }
