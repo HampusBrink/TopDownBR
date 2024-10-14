d6e7b143c7e154ddeb4dea81ff02b13cb14f8329
Raphael McCabe <raphaelmccabe123@gmail.com>
2024-10-14 17:00:33 +0200

    Added reverse walk for adjacent direction to opposite

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -161,12 +161,16 @@ namespace Player
             //UpdateAttackDurations();
         }
         
-        bool IsOppositeDirection(TurnDirection direction1, TurnDirection direction2)
+        bool IsOppositeOrAdjacentDirection(TurnDirection hitDirection, TurnDirection moveDirection)
         {
-            int difference = Mathf.Abs((int)direction1 - (int)direction2);
-            return difference == 4 || difference == 4 - 8 || difference == 4 + 8;
+            int hitValue = (int)hitDirection;
+            int moveValue = (int)moveDirection;
+            int difference = (hitValue - moveValue + 8) % 8; // Ensure positive difference
+
+            // Check if the difference is 3, 4, or 5 (opposite or adjacent)
+            return difference >= 3 && difference <= 5;
         }
-        
+
         void PlayAttackAnimation()
         {
             if (equippedWeapon.isAttacking)
@@ -174,18 +178,11 @@ namespace Player
                 _playerMovement.bodyAnim.SetBool("IsAttacking", true);
                 _playerMovement.bodyAnim.SetFloat("AttackX", _playerMovement.TurnDirectionToVector2(_hitDirection).x);
                 _playerMovement.bodyAnim.SetFloat("AttackY", _playerMovement.TurnDirectionToVector2(_hitDirection).y);
-            
+    
                 _playerMovement.legsAnim.SetFloat("MoveX", _playerMovement.TurnDirectionToVector2(_playerMovement.currentMoveDirection).x);
                 _playerMovement.legsAnim.SetFloat("MoveY", _playerMovement.TurnDirectionToVector2(_playerMovement.currentMoveDirection).y);
-            
-                if (IsOppositeDirection(_hitDirection, _playerMovement.currentMoveDirection))
-                {
-                    _playerMovement.legsAnim.SetBool("Reverse", true);
-                }
-                else
-                {
-                    _playerMovement.legsAnim.SetBool("Reverse", false);
-                }
+    
+                _playerMovement.legsAnim.SetBool("Reverse", IsOppositeOrAdjacentDirection(_hitDirection, _playerMovement.currentMoveDirection));
             }
             else
             {
@@ -194,7 +191,6 @@ namespace Player
                 _playerMovement.bodyAnim.SetFloat("AttackY", 0);
                 _playerMovement.legsAnim.SetBool("Reverse", false);
             }
-            
         }
     }
 }
