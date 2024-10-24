536a36f569ada5db093368d1efb2e82ba1c273c9
HampusBrink <hampus03brink@gmail.com>
2024-10-24 17:22:02 +0200

    testing testing

--- Assets/Scripts/NetworkRelated/ServerManager.cs
+++ Assets/Scripts/NetworkRelated/ServerManager.cs
@@ -11,38 +11,42 @@ namespace NetworkRelated
 {
     public class ServerManager : NetworkBehaviour
     {
-        EventBasedNetListener eventListener = new ();
 
         private void Start()
         {
             NetworkManager.TransportManager.Transport.OnClientConnectionState += Transport_OnClientConnectionState;
-            ServerManager.OnRemoteConnectionState += ServerManagerOnOnServerConnectionState;
-            
+            NetworkManager.ServerManager.OnRemoteConnectionState += OnServerRemoteConnectionState;
         }
 
-        private void ClientManagerOnOnClientConnectionState(ClientConnectionStateArgs obj)
+        private void OnServerRemoteConnectionState(NetworkConnection arg1, RemoteConnectionStateArgs arg2)
         {
-            
-        }
-
-        private void EventListenerOnPeerDisconnectedEvent(NetPeer peer, DisconnectInfo disconnectinfo)
-        {
-            Debug.Log("hejsan");
-        }
-
-        private void ServerManagerOnOnServerConnectionState(NetworkConnection networkConnection, RemoteConnectionStateArgs remoteConnectionStateArgs)
-        {
-            print("Client Left");
-            SteamMatchmaking.RequestLobbyData(new CSteamID(SteamManager.Instance.CurrentLobbyID));
+            print("Client Left!!!");
         }
 
         private void Transport_OnClientConnectionState(ClientConnectionStateArgs obj)
         {
+            //if(obj.ConnectionState != LocalConnectionState.Stopped) return;
             print("Client Left");
+            print(NetworkManager.ClientManager.Connection.Objects.Count);
             foreach (NetworkObject networkObject in NetworkManager.ClientManager.Connection.Objects)
+            {
+                print(networkObject.gameObject.name + "Got Destroyed");
                 networkObject.RemoveOwnership();
+            }
             
             SteamMatchmaking.RequestLobbyData(new CSteamID(SteamManager.Instance.CurrentLobbyID));
+        }
+        
+        public override void OnStartServer()
+        {
+            ServerManager.Objects.OnPreDestroyClientObjects += Objects_OnPreDestroyClientObjects;
+        }
+
+        private void Objects_OnPreDestroyClientObjects(NetworkConnection conn)
+        {
+            print("YEEPERS");
+            foreach (NetworkObject networkObject in conn.Objects)
+                networkObject.RemoveOwnership();
         }
 
         public void LeaveGame()
